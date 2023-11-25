7fbce6b1b02351dfd14456e53b90f6dabfa3db9c
sAndreas19 <hasansihombing0709@gmail.com>
2023-11-25 18:05:18 +0700

    Menambahkan modal untuk pembayaran

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,12 @@ btnBeli.forEach(item => {
     item.addEventListener('click', (e) => {
         let parent = e.target.parentNode.parentNode;
         let gambar = parent.querySelector('.card-img-top').src;
+        let harga = parent.querySelector('.harga').innerHTML;
+        let namaBarang = parent.querySelector('.card-text').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML : '<i>Informasi tidak tersedia</i>'
         console.log(`gambar: ${gambar}`);
+        console.log(`gambar: ${harga}`);
+        console.log(`gambar: ${namaBarang}`);
+        console.log(`gambar: ${deskripsi}`);
     });
 });
